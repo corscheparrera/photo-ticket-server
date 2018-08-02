bd52bc2541258c4398b9d93455673707939200e9
Maxime Lussier <m.lussier@live.ca>
2018-08-02 12:38:58 -0400

    remove cors

--- server/index.js
+++ server/index.js
@@ -33,14 +33,14 @@ configureServer(app);
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 // Serve front-end code
 app.use("/", express.static(path.join(__dirname, "../build")));
